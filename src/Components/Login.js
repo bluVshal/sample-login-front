1b6ae003ba884c2d5869f4a443dbe710631f9703
bluVshal <blu.vishal@gmail.com>
2024-07-19 14:06:26 +0400

    Modified Validation for SignIN and SignUp

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,21 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [errors, setErrors] = useState({
+        errors:[]
+    });
+
+    const Validation = (values) => {
+        let error = {};
+        const email_pattern = /^[^\s@]+@[^\s]+\.[^\s]+$/
+        const pass_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+
+        if(values.email === ""){
+            error.email = "Please enter your email"
+        }
+        else if(!email_pattern.test(values.email)){
+            error.email = "Please enter a valid email";
+        }
+
+        if(values.password === ""){
+            error.password = "Please enter your password";
+        }
+        else if(!pass_pattern.test(values.password)){
+            error.password = "Please enter a valid password";
+        }
+
+        return error;
+
+    };
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();       
+        setErrors(Validation(values));
+    };
+
+    const handleInput = (event) => {
+        setValues(prev => ({ ...prev, [event.target.name]: [event.target.value]}))
+    };
+
     return(
         <div className='d-flex justify-content-center align-items-center bg-black vh-100'>
             <div className='bg-white p-3 rounded w-25'>
+                <h2> Login </h2>
                 <form action="">
                     <div className='mb-3'>
                         <label htmlFor='email'><b>Email</b></label>
-                        <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>
+                        <input name='email' type="email" onInput={handleInput} placeholder='Enter Email' className='form-control rounded-0'/>
+                        <span>{errors.email && <span className='text-danger'> {errors.email} </span>}</span>
                     </div>
                     <div className='mb-3'>
                         <label htmlFor='password'><b>Password</b></label>
-                        <input type='password' placeholder='Enter Password' className='form-control rounded-0'></input>
+                        <input name='password' type='password' onInput={handleInput} placeholder='Enter Password' className='form-control rounded-0'></input>
+                        <span>{errors.password && <span className='text-danger'>{errors.password}</span>}</span>
                     </div>
-                    <button className='btn btn-success w-100'><u>L</u>og In</button>
                     <p> Do you agree to our terms and policies? </p>
+                    <button type="submit" onClick={handleSubmit} className='btn btn-success w-100'><u>L</u>og In</button>
+                    <p> Are you new here? Create your account</p>
                     <Link to='/signup' className='btn btn-primary border w-100'><u>C</u>reate Account</Link>
                 </form>
             </div>
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,9 +1,67 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 const Signup = () =>{
+    
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [errors, setErrors] = useState({
+        errors:[]
+    });
+    
+    const Validation = (values) => {
+        
+        let error = {};
+        const email_pattern = /^[^\s@]+@[^\s]+\.[^\s]+$/
+        const pass_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+
+        if(values.email === ""){
+            error.email = "Please enter your email"
+        }
+        else if(!email_pattern.test(values.email)){
+            error.email = "Please enter a valid email";
+        }
+
+        if(values.password === ""){
+            error.password = "Please enter your password";
+        }
+        else if(!pass_pattern.test(values.password)){
+            error.password = "Please enter a valid password";
+        }
+
+        return error;
+
+    };
+
     return(
-        <div>
-            
+        <div className='d-flex justify-content-center align-items-center bg-black vh-100'>
+            <div className='bg-white p-3 rounded w-25'>
+                <h2> Sign up</h2>
+                <form action="">
+                    <div className='mb-3'>
+                        <label htmlFor='email'><b>Name</b></label>
+                        <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='email'><b>Email</b></label>
+                        <input type="email" placeholder='Enter Email' className='form-control rounded-0'/>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='password'><b>Password</b></label>
+                        <input type='password' placeholder='Enter Password' className='form-control rounded-0'></input>
+                    </div>
+                    <div className='mb-3'>
+                        <label htmlFor='password'><b>Confirm Password</b></label>
+                        <input type='password' placeholder='Confirm Password' className='form-control rounded-0'></input>
+                    </div>
+                    <p> Do you agree to our terms and policies? </p>
+                    <button className='btn btn-success w-100'><u>S</u>ign up</button>
+                    <p> Already a member ? </p>
+                    <Link to='/' className='btn btn-primary border w-100'><u>L</u>og in</Link>
+                </form>
+            </div>
         </div>
     )
 }
